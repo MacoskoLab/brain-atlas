fb6cd0bea93ddf04c98442f9888d1eb3055db465
James Webber <jwebber@broadinstitute.org>
2021-08-09 23:20:45 -0400

    added gene selection code and pynndescent to env

---
+++ src/mouse_brain_atlas/gene_selection.py
@@ -0,0 +1,38 @@
+import dask.array as da
+
+import numpy as np
+import scipy.stats
+# blockwise poisson fit of gene counts
+
+
+def dask_pblock(ds: da.Array, b: int = 40000):
+    n_cells = ds.shape[0]
+
+    # pre-compute these values
+    pct = ((ds > 0).sum(0) / n_cells).compute()
+    exp = (ds.sum(0, keepdims=True) / ds.sum()).persist()
+    numis = ds.sum(1, keepdims=True).persist()
+
+    exp_pct_nz = np.zeros(exp.shape)  # 1 x n_genes
+    var_pct_nz = np.zeros(exp.shape)  # 1 x n_genes
+
+    # run in chunks (still large, but seems easier for dask to handle)
+    for i in range(0, n_cells, b):
+        prob_zero = da.exp(-exp.T.dot(numis[i: i + b, :].T))  # n_genes x b
+
+        exp_pct_nz_b = (1 - prob_zero).sum(1)  # n_genes
+        var_pct_nz_b = (prob_zero * (1 - prob_zero)).sum(1)
+
+        exp_pct_nz_b, var_pct_nz_b = da.compute(exp_pct_nz_b, var_pct_nz_b)
+
+        exp_pct_nz += exp_pct_nz_b
+        var_pct_nz += var_pct_nz_b
+
+    exp_pct_nz = exp_pct_nz.flatten() / n_cells
+    var_pct_nz = var_pct_nz.flatten() / (n_cells * n_cells)
+    std_pct_nz = np.sqrt(var_pct_nz)
+
+    exp_p = np.zeros_like(pct)
+    ix = (std_pct_nz != 0).flatten()
+    exp_p[ix] = scipy.stats.norm.logcdf(pct[ix], loc=exp_pct_nz[ix], scale=std_pct_nz[ix])
+    return exp_pct_nz, pct, exp_p

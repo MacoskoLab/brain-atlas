aba41693e6e6f32d44b8aaeae8a320bf78cdb6ab
James Webber <jwebber@broadinstitute.org>
2022-01-10 20:56:47 -0500

    new tree collapse logic--inspect grandchildren for de rewrote cluster_nz code--faster but hard to update split sib and subtree DE calculations, we use sib more use numba for some utility functions for a small speedup

--- src/brain_atlas/find_genes.py
+++ src/brain_atlas/find_genes.py
@@ -1,22 +1,24 @@
 import logging
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import Dict, Sequence, Tuple, Union
 
 import dask.array as da
+import numba as nb
 import numpy as np
 
-import brain_atlas.util.tree as tree
 from brain_atlas import Key
 from brain_atlas.diff_exp import mannwhitneyu
+from brain_atlas.util.tree import MultiNode
 
 log = logging.getLogger(__name__)
 
 
+@nb.njit(fastmath=True)
 def calc_log_fc(
     count_array: np.ndarray,
     sum_array: np.ndarray,
-    group1: Sequence[int],
-    group2: Sequence[int],
+    group1: np.ndarray,
+    group2: np.ndarray,
 ) -> np.ndarray:
     left_n = count_array[group1].sum()
     left_mean = sum_array[group1, :].sum(axis=0) / left_n
@@ -30,11 +32,12 @@ def calc_log_fc(
     return log_fc
 
 
+@nb.njit(fastmath=True)
 def calc_nz(
     count_array: np.ndarray,
     nz_array: np.ndarray,
-    group1: Sequence[int],
-    group2: Sequence[int],
+    group1: np.ndarray,
+    group2: np.ndarray,
 ):
     left_n = count_array[group1].sum()
     left_nz = nz_array[group1, :].sum(axis=0) / left_n
@@ -45,11 +48,12 @@ def calc_nz(
     return left_nz, right_nz
 
 
+@nb.njit(fastmath=True)
 def calc_filter(
     cluster_counts: np.ndarray,
     cluster_nz: np.ndarray,
-    group1: Sequence[int],
-    group2: Sequence[int],
+    group1: np.ndarray,
+    group2: np.ndarray,
     max_nz_b: float,
     delta_nz: float,
 ):
@@ -72,28 +76,24 @@ def cluster_nz_dict(
     data: da.Array,
     clusters: np.ndarray,
     node_list: Sequence[Key],
-    cluster_nz_d: Dict[Key, np.ndarray] = None,
+    blocksize: int = 256000,
 ):
     """
-    Calculates the number of nonzero elements per cluster. If given, an existing
-    dictionary of arrays can be updated, under the assumption that the rest of the tree
-    is unchanged.
+    Calculates the number of nonzero elements per cluster.
     """
-    if cluster_nz_d is None:
-        cluster_nz_d = dict()
+    n_cells, n_genes = data.shape
 
-    k2i = {k: i for i, k in enumerate(node_list)}
+    cluster_nz_d = defaultdict(lambda: np.zeros(n_genes, dtype=np.uint32))
 
-    # first need to remove parents to update their nz array as well
-    for k in node_list:
-        if len(k) and k not in cluster_nz_d and k[:-1] in cluster_nz_d:
-            cluster_nz_d.pop(k[:-1])
+    log.debug("counting nonzero elements")
+    for i in range(0, n_cells, blocksize):
+        log.debug(f"{i} ...")
+        cluster_i = clusters[i : i + blocksize]
+        data = da.sign(data.counts[i : i + blocksize, :]).compute()
+        for j in np.unique(cluster_i):
+            cluster_nz_d[node_list[j]] += data[cluster_i == j, :].sum(0)
 
-    for k in node_list:
-        if k not in cluster_nz_d:
-            cluster_i = clusters == k2i[k]
-            if cluster_i.any():
-                cluster_nz_d[k] = da.sign(data[cluster_i, :]).sum(0).compute()
+    cluster_nz_d = dict(cluster_nz_d)
 
     return cluster_nz_d
 
@@ -101,14 +101,11 @@ def cluster_nz_dict(
 def de(
     ds: Union[np.ndarray, da.Array],
     clusters: np.ndarray,
-    group1: Sequence[int],
-    group2: Sequence[int],
+    group1: np.ndarray,
+    group2: np.ndarray,
     gene_filter: np.ndarray,
     subsample: int = None,
 ):
-    group1 = np.asarray(group1)
-    group2 = np.asarray(group2)
-
     c_a = np.isin(clusters, group1)
     c_b = np.isin(clusters, group2)
 
@@ -133,105 +130,187 @@ def de(
     return full_u, full_p
 
 
-def leiden_tree_diff_exp(
+def prep_de(clusters, node_list, cluster_nz_d, n_genes):
+    cluster_counts = Counter(clusters[clusters > -1])
+    n_nodes = len(node_list)
+    assert set(cluster_counts).issubset(range(n_nodes))
+    assert all(node_list[i] in cluster_nz_d for i in cluster_counts)
+
+    k2i = {k: i for i, k in enumerate(node_list)}
+
+    cluster_nz = np.zeros((n_nodes, n_genes))
+    for k in cluster_nz_d:
+        cluster_nz[k2i[k], :] = cluster_nz_d[k]
+
+    cluster_counts = np.array([cluster_counts[i] for i in range(n_nodes)])
+
+    return k2i, cluster_nz, cluster_counts
+
+
+def sibling_de(
     data: da.array,
     clusters: np.ndarray,
     node_list: Sequence[Key],
-    node_tree: Dict[Key, tree.MultiNode],
-    cluster_nz_d: Dict[Key, np.ndarray] = None,
+    node_tree: Dict[Key, MultiNode],
+    cluster_nz_d: Dict[Key, np.ndarray],
     sibling_results: Dict[Key, Tuple[np.ndarray, ...]] = None,
+    delta_nz: float = 0.2,
+    max_nz_b: float = 0.2,
+    subsample: int = None,
+    min_depth: int = 0,
+):
+    k2i, cluster_nz, cluster_counts = prep_de(
+        clusters, node_list, cluster_nz_d, data.shape[1]
+    )
+
+    def nd2i(nd):
+        return k2i[nd.node_id]
+
+    if sibling_results is None:
+        sibling_results = dict()
+
+    n_sib = len(sibling_results)
+
+    for k in node_list:
+        if len(k) < min_depth or node_tree[k].is_leaf:
+            continue
+
+        c_arrays = {
+            nd.node_id: np.array(nd.pre_order(True, nd2i))
+            for nd in node_tree[k].children
+        }
+
+        for n in node_tree[k].children:
+            i = n.node_id
+            if i in sibling_results:
+                continue
+
+            c_i = c_arrays[i]
+            c_j = np.hstack([c_arrays[j] for j in c_arrays if i != j])
+
+            # don't calculate if it's redundant with a 1-vs-all comp
+            if k == () and (len(c_i) == 1 or len(c_j) == 1):
+                continue
+
+            log.debug(f"Comparing {c_i} with {c_j}")
+            nz_diff, nz_filter = calc_filter(
+                cluster_counts, cluster_nz, c_i, c_j, max_nz_b, delta_nz
+            )
+
+            _, p = de(data, clusters, c_i, c_j, nz_filter, subsample)
+            sibling_results[i] = p, nz_diff, nz_filter
+
+            # common special case is 2 siblings: results are symmetrical
+            if len(node_tree[k].children) == 2:
+                j = node_tree[k].children[1].node_id
+                sibling_results[j] = p, -nz_diff, nz_filter
+                break
+
+    return sibling_results, len(sibling_results) - n_sib
+
+
+def subtree_de(
+    data: da.array,
+    clusters: np.ndarray,
+    node_list: Sequence[Key],
+    node_tree: Dict[Key, MultiNode],
+    cluster_nz_d: Dict[Key, np.ndarray],
     subtree_results: Dict[Key, Tuple[np.ndarray, ...]] = None,
     delta_nz: float = 0.2,
     max_nz_b: float = 0.2,
     subsample: int = None,
     min_depth: int = 0,
 ):
-    cluster_counts = Counter(clusters[clusters > -1])
-    n_nodes = len(node_list)
-    ni_set = set(range(n_nodes))
-    assert set(cluster_counts).issubset(ni_set)
+    nd2i, cluster_nz, cluster_counts = prep_de(
+        clusters, node_list, cluster_nz_d, data.shape[1]
+    )
 
-    k2i = {k: i for i, k in enumerate(node_list)}
-
-    def nd2i(nd):
-        return k2i[nd.node_id]
-
-    if cluster_nz_d is None:
-        cluster_nz_d = cluster_nz_dict(data, clusters, node_list)
-
-    assert all(node_list[i] in cluster_nz_d for i in cluster_counts)
-
-    cluster_nz = np.zeros((n_nodes, data.shape[1]))
-    for k in cluster_nz_d:
-        cluster_nz[k2i[k], :] = cluster_nz_d[k]
-
-    cluster_counts = np.array([cluster_counts[i] for i in range(n_nodes)])
-
-    if sibling_results is None:
-        sibling_results = dict()
     if subtree_results is None:
         subtree_results = dict()
 
-    n_sib = len(sibling_results)
     n_sub = len(subtree_results)
+    ni_set = set(range(len(node_list)))
 
     for k in node_list:
-        if len(k) < min_depth:
+        if k == () or len(k) < min_depth or k in subtree_results:
             continue
 
-        if not node_tree[k].is_leaf:
-            c_lists = {
-                nd.node_id: nd.pre_order(True, nd2i) for nd in node_tree[k].children
-            }
+        below = np.array(node_tree[k].pre_order(True, nd2i))
+        above = np.array(sorted(ni_set - set(below)))
 
-            for n in node_tree[k].children:
-                i = n.node_id
-                if i in sibling_results:
-                    continue
+        # don't calculate redundant comparison
+        if len(above) == 1:
+            continue
 
-                c_i = c_lists[i]
-                c_j = [c_o for j in c_lists if i != j for c_o in c_lists[j]]
+        log.debug(f"Comparing {below} with {above}")
+        nz_diff, nz_filter = calc_filter(
+            cluster_counts, cluster_nz, below, above, max_nz_b, delta_nz
+        )
 
-                # don't calculate if it's redundant with a 1-vs-all comp
-                if k == () and (len(c_i) == 1 or len(c_j) == 1):
-                    continue
+        _, p = de(data, clusters, below, above, nz_filter, subsample)
+        subtree_results[k] = p, nz_diff, nz_filter
 
-                log.debug(f"Comparing {c_i} with {c_j}")
-                nz_diff, nz_filter = calc_filter(
-                    cluster_counts, cluster_nz, c_i, c_j, max_nz_b, delta_nz
-                )
+    return subtree_results, len(subtree_results) - n_sub
 
-                _, p = de(data, clusters, c_i, c_j, nz_filter, subsample)
-                sibling_results[i] = p, nz_diff, nz_filter
 
-                # common special case is 2 siblings: results are symmetrical
-                if len(node_tree[k].children) == 2:
-                    j = node_tree[k].children[1].node_id
-                    sibling_results[j] = p, -nz_diff, nz_filter
-                    break
+def get_de_totals(
+    comp: Key,
+    diff_results: Dict[Key, Tuple[np.ndarray, ...]],
+    min_p: float = -10.0,
+):
+    if comp not in diff_results:
+        return 0, 0
 
-        if k != () and min_depth == 0:
-            if k in subtree_results:
+    p, nz_diff, nz_filter = diff_results[comp]
+
+    total_a = ((p < min_p) & (nz_diff > 0)).sum()
+    total_b = ((p < min_p) & (nz_diff < 0)).sum()
+
+    return total_a, total_b
+
+
+def collapse_tree(
+    node_list: Sequence[Key],
+    node_tree: Dict[Key, MultiNode],
+    sib_results: Dict[Key, Tuple[np.ndarray, ...]],
+    min_de: int = 5,
+    min_p: float = -10.0,
+):
+    sib_totals = {k: get_de_totals(k, sib_results, min_p=min_p) for k in sib_results}
+
+    exclude = set()
+    new_leaf_list = []
+
+    # assumes node_list is bottom-up. reverse it to start at root
+    for k in node_list[::-1]:
+        if k in exclude:
+            continue
+
+        if node_tree[k].is_leaf:
+            # if we get to a leaf node, it qualifies
+            new_leaf_list.append(k)
+        elif not all(nd.node_id in sib_totals for nd in node_tree[k].children):
+            # child nodes are not all tested, leave for now
+            continue
+        elif all(
+            any(t < min_de for t in sib_totals[nd.node_id])
+            for nd in node_tree[k].children
+        ):
+            # all child nodes fail DE test. but maybe grandchildren are interesting
+            grandchildren = [
+                all(t >= min_de for t in sib_totals[nd2.node_id])
+                for nd in node_tree[k].children
+                if not nd.is_leaf
+                for nd2 in node_tree[nd.node_id].children
+                if nd2.node_id in sib_totals
+            ]
+
+            if grandchildren and all(grandchildren):
+                # this level was boring, but grandchildren passed
                 continue
+            else:
+                # otherwise consider it a leaf and merge descendants
+                new_leaf_list.append(k)
+                exclude.update(node_tree[k].pre_order(True))
 
-            below = node_tree[k].pre_order(True, nd2i)
-            above = sorted(ni_set - set(below))
-
-            # don't calculate redundant comparison
-            if len(above) == 1:
-                continue
-
-            log.debug(f"Comparing {below} with {above}")
-            nz_diff, nz_filter = calc_filter(
-                cluster_counts, cluster_nz, below, above, max_nz_b, delta_nz
-            )
-
-            _, p = de(data, clusters, below, above, nz_filter, subsample)
-            subtree_results[k] = p, nz_diff, nz_filter
-
-    return (
-        sibling_results,
-        subtree_results,
-        len(sibling_results) - n_sib,
-        len(subtree_results) - n_sub,
-    )
+    return new_leaf_list
